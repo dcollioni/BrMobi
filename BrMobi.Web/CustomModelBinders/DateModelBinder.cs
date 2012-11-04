5d07f6fc7a991d0dd3d9d5db936e07487df1abd4
Douglas Collioni <dcollioni@gmail.com>
2012-11-03 23:26:06 -0200

    Date and Time model binders.

---
+++ BrMobi.Web/CustomModelBinders/DateModelBinder.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Web.Mvc;
+
+namespace BrMobi.Web.CustomModelBinders
+{
+    public class DateModelBinder : IModelBinder
+    {
+        public object BindModel(ControllerContext controllerContext, ModelBindingContext bindingContext)
+        {
+            var value = controllerContext.HttpContext.Request.Form[bindingContext.ModelName];
+            var dateValue = DateTime.Parse(value, new System.Globalization.CultureInfo("pt-BR"));
+
+            return dateValue;
+        }
+    }
+}
---
+++ BrMobi.Web/CustomModelBinders/TimeModelBinder.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Web.Mvc;
+
+namespace BrMobi.Web.CustomModelBinders
+{
+    public class TimeModelBinder : IModelBinder
+    {
+        public object BindModel(ControllerContext controllerContext, ModelBindingContext bindingContext)
+        {
+            var value = controllerContext.HttpContext.Request.Form[bindingContext.ModelName];
+            var timeValue = TimeSpan.Parse(value, new System.Globalization.CultureInfo("pt-BR"));
+
+            return timeValue;
+        }
+    }
+}
